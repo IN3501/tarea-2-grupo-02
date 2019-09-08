f54ce61e1b98e75f9c0f2cf2656240964a77b7d3
kyra <kyra.cossio@ug.uchile.cl>
2019-09-08 13:06:12 -0300

    arreglar algunos bugs

--- T2G02/MiApp/views.py
+++ T2G02/MiApp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+	return render(request, 'MiApp/index.html')
+
+def pestaña(request):
+	return render(request, 'MiApp/pestaña.html')
