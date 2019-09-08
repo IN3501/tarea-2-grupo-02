bb16054f1e6d54eacb92eda14748adf9cf7834e2
kyra <kyra.cossio@ug.uchile.cl>
2019-09-08 13:57:29 -0300

    agregar base

--- T2G02/MiApp/urls.py
+++ T2G02/MiApp/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from . import views
+from .views import *
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('pestaña/', views.pestaña ,name='pestaña'),
+    path('', index, name='index'),
+    path('pestaña/', pestaña ,name='pestaña'),
+    path('inputs/', inputs, name='inputs'),
+    path('formulario/', form, name='form'),
 ]
 
--- T2G02/MiApp/views.py
+++ T2G02/MiApp/views.py
@@ -2,7 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-	return render(request, 'MiApp/index.html')
+	return render(request, 'home.html')
 
 def pestaña(request):
-	return render(request, 'MiApp/pestaña.html')
+	return render(request, 'pestaña.html')
+
+def inputs(request):
+	return render(request, 'inputs.html')
+
+def form(request):
+	return render(request, 'form.html')
