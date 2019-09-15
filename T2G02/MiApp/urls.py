35a1b71f53ed394c3cbafa9e7765868126a504a6
unknown <paulaberrios1996@hotmail.com>
2019-09-15 21:57:49 +0200

    formularios de contacto, mensajes e inbox

--- T2G02/MiApp/urls.py
+++ T2G02/MiApp/urls.py
@@ -3,11 +3,15 @@ from django.urls import path
 from .views import *
 urlpatterns = [
     path('', index, name='index'),
-    path('pestaña/', pestaña ,name='pestaña'),
+    path('Contacto/', contacto ,name='contacto'),
     path('inputs/', inputs, name='inputs'),
     path('formulario/', form, name='form'),
     path('registrarse/', registrarse, name='registrarse'),
     path('exitoCrearCuenta/', exitoCrearCuenta, name='exitoCrearCuenta'),
+    path('nuevoMensaje/', nuevoMensaje, name='nuevoMensaje'),
+    path('inbox/', inbox, name='inbox'),
+    path('contactoExitoso/', contactoExitoso, name='contactoExitoso'),
+    path('mensajeEnviado/', mensajeEnviado, name="mensajeEnviado"),
 
 ]
 
--- T2G02/MiApp/views.py
+++ T2G02/MiApp/views.py
@@ -4,8 +4,11 @@ from django.shortcuts import render
 def index(request):
 	return render(request, 'home.html')
 
-def pestaña(request):
-	return render(request, 'pestaña.html')
+def contacto(request):
+	if request.method =='POST':
+		return render(request, 'contactoExitoso.html')
+	else:
+		return render(request, 'Contacto.html')
 
 def inputs(request):
 	return render(request, 'inputs.html')
@@ -18,6 +21,28 @@ def registrarse(request):
 		return render(request, 'mensajeDeExito.html')
 	else:
 		return render(request, 'formNuevaCuenta.html')
+
+def contactoExitoso(request):
+	if request is not None:
+		if request.method == 'POST':
+			if 'inputText' and 'inputEmail' and 'Coment':
+				nombre=request.POST['inputText']
+				email=request.POST['inputEmail']
+				coment=request.POST['Coment']
+				return render(request, 'contactoExitoso.html')
+	return render(request, 'contacto.html')
+
+def mensajeEnviado(request):
+	if request is not None:
+		if request.method == 'POST':
+			if 'inputText' and 'inputEmail' and 'inputEmail2' and 'quien'and'Coment' :
+				nombre=request.POST['inputText']
+				email=request.POST['inputEmail']
+				destino=request.POST['quien']
+				email=request.POST['inputEmail2']
+				coment=request.POST['Coment']
+				return render(request, 'mensajeEnviado.html')
+	return render(request, 'nuevoMensaje.html')
 
 def exitoCrearCuenta(request):
 	if request is not None:
@@ -33,3 +58,12 @@ def exitoCrearCuenta(request):
 									  'message': mensaje})
 
 	return render(request, 'formNuevaCuenta.html')
+
+def nuevoMensaje(request):
+	if request.method == 'POST':
+		return render(request, 'mensajeEnviado.html')
+	else:
+		return render(request, 'nuevoMensaje.html')
+
+def inbox(request):
+	return render(request, 'inbox.html')
