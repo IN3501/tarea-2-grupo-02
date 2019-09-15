0ed3bfbfd52ef2363b9f464795dc2f1cc4ef6320
kyra <kyrantonia@gmail.com>
2019-09-15 15:06:48 -0300

    Agregar más vistas

--- T2G02/MiApp/urls.py
+++ T2G02/MiApp/urls.py
@@ -2,12 +2,14 @@ from django.contrib import admin
 from django.urls import path
 from .views import *
 urlpatterns = [
-    path('', index, name='index'),
-    path('pestaña/', pestaña ,name='pestaña'),
-    path('inputs/', inputs, name='inputs'),
-    path('formulario/', form, name='form'),
+    path('', login, name='login'),
+    path('index/', index, name='index'),
+    path('avisos/', avisos, name='avisos'),
     path('registrarse/', registrarse, name='registrarse'),
     path('exitoCrearCuenta/', exitoCrearCuenta, name='exitoCrearCuenta'),
+    path('agregarAviso/', agregarAviso, name='agregarAviso'),
+    path('feedback/', feedback, name='feedback'),
+    path('exitofeedback/', exitoFeedback, name='exitofeedback'),
 
 ]
 
--- T2G02/MiApp/views.py
+++ T2G02/MiApp/views.py
@@ -1,35 +1,69 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def index(request):
-	return render(request, 'home.html')
+    if request is not None:
+        if request.method == 'POST':
+            if 'inputText' and 'password':
+                nombre = request.POST['inputText']
+                if nombre:
+                    return render(request, 'home.html',
+                                  {'name': nombre}
+                                   )
+    return render(request, 'home.html')
+def login(request):
+    return render(request, 'login.html')
+def avisos(request):
+    if request.method == 'POST':
+        name = request.POST['inputText']
+        type = request.POST['type']
+        day = request.POST.getlist("day")
+        s = ", "
+        if name and type and day:
+            return render(request, 'avisos.html',
+                          {'name': name,
+                           'type':type,
+                           'day':s.join(day)
+                           })
+    return render(request, 'avisos.html')
 
-def pestaña(request):
-	return render(request, 'pestaña.html')
-
-def inputs(request):
-	return render(request, 'inputs.html')
-
-def form(request):
-	return render(request, 'form.html')
 
 def registrarse(request):
-	if request.method == 'POST':
-		return render(request, 'mensajeDeExito.html')
-	else:
-		return render(request, 'formNuevaCuenta.html')
+    if request.method == 'POST':
+        return render(request, 'mensajeDeExito.html')
+    else:
+        return render(request, 'formNuevaCuenta.html')
+def feedback(request):
+    return render(request,'formulariotarea2.html')
+def exitoFeedback(request):
+    if request is not None:
+        if request.method == 'POST':
+            if 'inputText' and 'inputEmail' and 'inputPassword':
+                nombre = request.POST['inputText']
+                materia = request.POST['materia']
+                descripcion = request.POST['descripcion']
+                if nombre and materia and descripcion:
+                    mensaje = "Hemos enviado tu feedback a " + nombre + ", con quien tuviste clases de  " + \
+                              materia+" con el siguiente comentario "+descripcion
+                    return render(request, 'mensajeDeExitoLogOut.html',
+                                  {'titleMessage': "Feedback enviado con exito",
+                                   'message': mensaje})
+    return render(request,'formulariotarea2.html')
 
 def exitoCrearCuenta(request):
-	if request is not None:
-		if request.method =='POST':
-			if 'inputText' and 'inputEmail' and 'inputPassword':
-				nombre =request.POST['inputText']
-				email =request.POST['inputEmail']
-				password = request.POST['inputPassword']
-				if nombre and email and password:
-					mensaje="Felicidades "+nombre+", se ha creado tu cuenta con el correo " + email
-					return render(request, 'mensajeDeExitoLogOut.html',
-								  {'titleMessage':"Cuenta creada con exito",
-									  'message': mensaje})
+    if request is not None:
+        if request.method == 'POST':
+            if 'inputText' and 'inputEmail' and 'inputPassword':
+                nombre = request.POST['inputText']
+                email = request.POST['inputEmail']
+                password = request.POST['inputPassword']
+                if nombre and email and password:
+                    mensaje = "Felicidades " + nombre + ", se ha creado tu cuenta con el correo " + email
+                    return render(request, 'mensajeDeExitoLogOut.html',
+                                  {'titleMessage': "Cuenta creada con exito",
+                                   'message': mensaje})
 
-	return render(request, 'formNuevaCuenta.html')
+    return render(request, 'formNuevaCuenta.html')
+def agregarAviso(request):
+    return render(request, 'formAgregarAviso.html')
